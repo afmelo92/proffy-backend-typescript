5c4d4324b2ace566f3784c6314bac879f5798285
Andre Melo <andre.fabian.melo@gmail.com>
2020-08-04 17:49:26 -0300

    Proffy server users and classes CRUD

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,13 @@
 import express from 'express';
+import cors from 'cors';
+
+import routes from './routes';
 
 const app = express();
 
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get(`/`, (request, response) => {
-  return response.json({
-    ok: true
-  })
-})
+app.use(routes);
 
-app.listen(3333)
+app.listen(3333);
